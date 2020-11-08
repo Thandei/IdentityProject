ae9324cc7540e5697df75bc6b21319771897a90a
ufuky <ufuky@LAPTOP-LRLCM4LR>
2020-11-08 17:24:33 +0300

    Add project files.

---
+++ IdentityProject/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using IdentityProject.Areas.Identity.Data;
+using IdentityProject.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(IdentityProject.Areas.Identity.IdentityHostingStartup))]
+namespace IdentityProject.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IdentityDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("IdentityDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+
+                    
+                    })
+                    .AddEntityFrameworkStores<IdentityDbContext>();
+            });
+        }
+    }
+}
